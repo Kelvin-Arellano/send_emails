19075398b78f012c06a1c6e36b3aa9704704eb71
Kelvin Arellano <55723330+Kelvin-Arellano@users.noreply.github.com>
2022-02-23 20:32:11 +0000

    modified the code to send every 60 sec

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import yagmail
 import os
+import time
 
-sender = os.environ['EMAIL']
-receiver = 'kepiba7946@sueshaw.com'
-subject = 'This is the subject'
-contents = '''
-  This is what I am going to send to you shut up and enjoy it.
-'''
 
-yag = yagmail.SMTP(user=sender,password=os.environ['PASSWORD'])
-yag.send(to=receiver, subject=subject, contents=contents)
-print('Email sent')
+while True:
+  
+  sender = os.environ['EMAIL']
+  receiver = 'kepiba7946@sueshaw.com'
+  subject = 'This is the subject'
+  contents = '''
+    This is what I am going to send to you shut up and enjoy it.
+  '''
+  
+  yag = yagmail.SMTP(user=sender,password=os.environ['PASSWORD'])
+  yag.send(to=receiver, subject=subject, contents=contents)
+  print('Email sent')
+  time.sleep(60)
